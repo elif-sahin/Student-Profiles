ee4b22d08b864cd110de2f4c9b646cb455c1678b
elif-sahin <sahinelif.mail@gmail.com>
2022-03-14 22:29:21 +0300

    expand feature added

--- student-profiles/src/ProfileInfoCard/ProfileInfoCard.tsx
+++ student-profiles/src/ProfileInfoCard/ProfileInfoCard.tsx
@@ -1,3 +1,4 @@
+import React from "react";
 import { CircleImage } from "../CircleImage/CircleImage";
 import { IStudentInfo } from "../ProfileList/Api";
 import "./profileInfoCard.scss";
@@ -6,18 +7,32 @@ interface IProfileInfoCardProps {
     profile: IStudentInfo;
 }
 export const ProfileInfoCard = (props: IProfileInfoCardProps) => {
+    const [isExpanded, setIsExpanded] = React.useState(false);
+
+    const onExpand = () => {
+        setIsExpanded(!isExpanded);
+    }
+
+    const renderGrades = () => props.profile.grades.map((grade, index) => <div className="test-result">{"Test" + (index + 1) + ": " + grade}</div>)
+
     return (
         <div className="card-wrapper">
             <div className="card-left">
                 <CircleImage source={props.profile.pic}></CircleImage>
             </div>
             <div className="card-right">
-                <div className="card-header">{props.profile.firstName + " " + props.profile.lastName}</div>
+                <div className="header-wrapper">
+                    <div className="card-header">{props.profile.firstName + " " + props.profile.lastName}</div>
+                    <button className="expand-button" onClick={onExpand}>{(isExpanded ? "-" : "+")}</button>
+                </div>
                 <div className="card-info">{"Email: " + props.profile.email}</div>
                 <div className="card-info">{"Company: " + props.profile.company}</div>
                 <div className="card-info">{"Skill: " + props.profile.skill}</div>
                 <div className="card-info">{"Avarage: " + 100}</div>
+
+                {isExpanded && <div className="card-expand">{renderGrades()}</div>}
             </div>
+
         </div>
     );
 }
